c9224c6f97260f02b775bca1f4bde86bc7a42774
Pedro Vituri <pedrovituri@gmail.com>
2023-02-22 17:43:16 -0300

    troubleshooting loop

--- script.js
+++ script.js
@@ -1,42 +1,48 @@
-function getComputerChoice() {
-    let computerSelection 
-    computerSelection = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
-    return computerSelection
-}
-
-let computerSelection = getComputerChoice()
-
 let playerSelection = 2
 playerScore = 0
 computerScore = 0
+let computerSelection
 
 function playRound(playerSelection, computerSelection) {
+    let computerSelector = function getComputerChoice() {
+        let computerMath = Math.floor(Math.random() * 3) + 1;
+        return computerMath
+    }
+    computerSelection = computerSelector
     if (playerSelection == computerSelection) {
     console.log('it\'s a tie');
     roundWinner = 'tie';
+    console.log(computerSelection);
+    console.log(playerSelection);
 } else if ((playerSelection == 1 && computerSelection == 3) || 
           (playerSelection == 2 && computerSelection == 1) || 
           (playerSelection == 3 && computerSelection == 2)) {
     console.log('You won this round');
     playerScore++
     roundWinner = 'player';
+    console.log(computerSelection)
+    console.log(playerSelection)
 } else if ((playerSelection == 1 && computerSelection == 2) ||
           (playerSelection == 2 && computerSelection == 3) ||
           (playerSelection == 3 && computerSelection == 1)) {
     console.log('You lost this round');
     computerScore++
     roundWinner = 'computer';
+    console.log(computerSelection)
+    console.log(playerSelection)
 }}
 
-function playGame(playerSelection, computerSelection) {
-    for (let i = 0; computerSelection <= 3 || playerSelection <= 3; i++) {
-    playRound(playerSelection, computerSelection)
+function playGame() {
+    for (let i = 0; i <= 5 ; i++) {
+    playRound(playerSelection, computerSelection);
     if (playerScore = 3) {
         console.log('You won the game');
-    } else if (computerScore = 3) {
+    }
+    if (computerScore = 3) {
         console.log('The computer won the game');
-    }}}
+    }
+}
+}
 
-console.log(computerSelection)
-console.log(playerSelection)
-console.log(playGame(playerSelection, computerSelection))
+console.log(playRound(playerSelection, computerSelection))
+console.log(playGame())
